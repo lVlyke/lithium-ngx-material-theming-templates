2372cad1ccb622169f564980f3d58a0bf2e54912
Mychal Thompson <mychalrthompson@gmail.com>
2023-04-08 17:01:22 -0400

    0.9.0: - Adds support for Angular 15. - Adds theming templates for legacy (pre-v15) Angular Material themes.

--- scripts/compile-templates.js
+++ scripts/compile-templates.js
@@ -3,4 +3,6 @@ const compileTemplate = require("./compile-template");
 module.exports = function compileTemplates() {
     compileTemplate("standard-light-theme");
     compileTemplate("standard-dark-theme");
+    compileTemplate("legacy-light-theme");
+    compileTemplate("legacy-dark-theme");
 };
