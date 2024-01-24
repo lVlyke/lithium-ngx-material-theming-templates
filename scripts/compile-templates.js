d32e89a3528ac963cebdbb8a61c5008e71a9a619
Mychal Thompson <mychalrthompson@gmail.com>
2024-01-23 19:57:32 -0500

    0.12.0: Dropped legacy component support.

--- scripts/compile-templates.js
+++ scripts/compile-templates.js
@@ -3,6 +3,4 @@ const compileTemplate = require("./compile-template");
 module.exports = function compileTemplates() {
     compileTemplate("standard-light-theme");
     compileTemplate("standard-dark-theme");
-    compileTemplate("legacy-light-theme");
-    compileTemplate("legacy-dark-theme");
 };
